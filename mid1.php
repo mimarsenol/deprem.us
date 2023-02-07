8cefc1f4568dd840a967a27823781421982ce584
Şenol GÖKNER <senol.gokner@gmail.com>
2023-02-07 16:38:53 +0300

    kurulum-5

--- mid1.php
+++ mid1.php
@@ -1,3 +1,3 @@
 <h3 >Deprem Koordinasyon Haritası</h3>
     <div class="con">
-    <iframe src="https://www.google.com/maps/d/embed?mid=1zmP2Vw9sKqrQg4ILv931txlrJssZyV0&ehbc=2E312F" width="100%" height="480"></iframe>
+    <iframe src="https://www.google.com/maps/d/embed?mid=1zmP2Vw9sKqrQg4ILv931txlrJssZyV0&ehbc=2E312F" width="100%" height="600"></iframe>
