e927f4eb9562fad6fe38b63f535403156f54bfa3
Şenol GÖKNER <senol.gokner@gmail.com>
2023-02-08 12:45:24 +0300

    Guncelleme12

--- mid2.php
+++ mid2.php
@@ -3,21 +3,21 @@
         <div class="col-3">    <div class="card ">
       <img src="img/yas.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h1 class="card-title center">5894</h1>
+        <h1 class="card-title center">7108</h1>
         <p class="card-text center">Vatandaşımızı kaybettik.</p>
       </div>
       <div class="card-footer">
-        <small class="text-muted ">08.02.2023 00:00</small>
+        <small class="text-muted ">08.02.2023 12:45</small>
       </div>
     </div></div>
         <div class="col-3">    <div class="card ">
       <img src="img/yarali-2.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h1 class="card-title center">34810</h1>
+        <h1 class="card-title center">40910</h1>
         <p class="card-text center">Yaralı vatandaşımız var</p>
       </div>
       <div class="card-footer">
-        <small class="text-muted ">08.02.2023 00:00</small>
+        <small class="text-muted ">08.02.2023 12:45</small>
       </div>
     </div></div>
         <div class="col-3">    <div class="card ">
@@ -27,7 +27,7 @@
         <p class="card-text center">Binamız yıkıldı.</p>
       </div>
       <div class="card-footer">
-        <small class="text-muted ">08.02.2023 00:00</small>
+        <small class="text-muted ">08.02.2023 12:45</small>
       </div>
     </div></div>
         <div class="col-3">    <div class="card ">
