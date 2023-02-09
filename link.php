0f64dd41e57e1e483e7fff8363da75dc662a0ab7
Şenol GÖKNER <senol.gokner@gmail.com>
2023-02-09 18:32:51 +0300

    fix

--- link.php
+++ link.php
@@ -2,7 +2,7 @@
 <div class="container">
     <div class="row">
         <div class="rounded border center m-1"><a href="https://www.afetbilgi.com/"> <p>Afet Bilgi Sitesi</p></a></div>
-        <div class="rounded border center m-1 "><a href="https://evimmusait.com/ "> <p>Evim Müsait -Barınma Destek Platformu/p></a></p></div>
+        <div class="rounded border center m-1 "><a href="https://evimmusait.com/ "> <p>Evim Müsait -Barınma Destek Platformu</p></a></div>
         <div class="rounded border center m-1"><a href="https://www.dempre.io"> <p>Derem.io Yardım ve Destek talepleri</p></a> </div>
         <div class="rounded border center m-1"><a href="https://deprem.basarsoft.com.tr/V2#"> <p>Afet Bilgi Deprem Durum -Talep Haritası</p></a> </div>
     </div>
