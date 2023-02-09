2d40a60f00624cfc3f8e7ff183b94f508cceb503
Şenol GÖKNER <senol.gokner@gmail.com>
2023-02-09 14:40:28 +0300

    fix

--- mid2.php
+++ mid2.php
@@ -1,6 +1,6 @@
 <div class="container mt-1 mb-4">
     <div class="row">
-        <div class="col-3 shadow-sm p-3 mb-5 bg-body rounded">    <div class="card ">
+        <div class=" shadow-sm p-3 mb-5 bg-body rounded col-sm-6 col-lg-3  ">    <div class="card ">
       <img src="img/yas.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h1 class="card-title center bg-danger">14014</h1>
@@ -10,7 +10,7 @@
         <small class="text-muted ">08.02.2023 21:31</small>
       </div>
     </div></div>
-        <div class="col-3 shadow-sm p-3 mb-5 bg-body rounded">    <div class="card ">
+        <div class=" shadow-sm p-3 mb-5 bg-body rounded col-sm-6 col-lg-3 ">    <div class="card ">
       <img src="img/yarali-2.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h1 class="card-title center bg-warning">63794</h1>
@@ -20,17 +20,17 @@
         <small class="text-muted ">08.02.2023 21:31</small>
       </div>
     </div></div>
-        <div class="col-3  shadow-sm p-3 mb-5 bg-body rounded">    <div class="card ">
+        <div class="  shadow-sm p-3 mb-5 bg-body rounded col-sm-6 col-lg-3  ">    <div class="card ">
       <img src="img/bina.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h1 class="card-title center bg-secondary">6444</h1>
+        <h1 class="card-title center bg-secondary">12444</h1>
         <p class="card-text center">Binamız yıkıldı.</p>
       </div>
       <div class="card-footer">
         <small class="text-muted ">08.02.2023 21:31</small>
       </div>
     </div></div>
-        <div class="col-3 shadow-sm p-3 mb-5 bg-body rounded">    <div class="card ">
+        <div class=" shadow-sm p-3 mb-5 bg-body rounded col-sm-6 col-lg-3  ">    <div class="card ">
       <img src="img/deprem.png" class="card-img-top" alt="...">
       <div class="card-body"> 
         <h1 class="card-title center bg-info ">7.7 </h1>
